3face9a3e8d11723e5914e8727d4a5e9c1e15144
Pranay Rajana <pranayarajana.97@gmail.com>
2023-11-28 22:07:21 -0600

    changes in Qrcode

---
+++ frontend/src/components/User/UserHome/Qrcodeone.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Qrcodeone() {
+  return (
+    <div>
+Eagle Point<br/>
+Longitude : -97.022238<br/>
+Latitude : 33.375827<br/>
+Rating : 1<br/>
+Status : available<br/>
+Baby Changing Station : Available<br/>
+Handicap Availability : Yes<br/>
+    </div>
+  )
+}
+
+export default Qrcodeone;
---
+++ frontend/src/components/User/UserHome/Qrcodethree.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Qrcodethree() {
+  return (
+    <div>
+        Parrot View<br/>
+Longitude : 79.8612<br/>
+Latitude : 15.591<br/>
+Rating : 1<br/>
+Status : available<br/>
+Baby Changing Station : Available<br/>
+Handicap Availability : Yes<br/>
+    </div>
+  )
+}
+
+export default Qrcodethree;
